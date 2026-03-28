1d6539c2f13dbf46945ad7fd545aa6c0b05e6cc3
Ngoc Quan <ngocquanofficial@gmail.com>
2026-03-28 15:24:11 +0700

    finish cross entropy loss

--- assignment1/cs336_basics/loss.py
+++ assignment1/cs336_basics/loss.py
@@ -10,13 +10,15 @@ class CrossEntropyLoss(nn.Module) :
         Notice that CE(x, target_idx) = - log softmax(x)[target_idx]
                                           = log(sum(e**x)) - log e**x[i]) = log(sum(e**x)) - x[i]
         """
+        target = target.to(torch.int64)
         # Minus max value for numerical stability
-        scale_logits = logits - torch.max(logits, dim= -1, keepdim= True).values 
+        logits = logits - torch.max(logits, dim= -1, keepdim= True).values
+        # breakpoint() 
         
-        e_logits = torch.exp(scale_logits)
+        e_logits = torch.exp(logits)
         log_sum = torch.log(torch.sum(e_logits, dim=-1, keepdim= False))
         logits_idx = logits.gather(dim=-1, index= target.unsqueeze(-1)).squeeze(-1)
-
+        # breakpoint()
         total_loss = log_sum - logits_idx
         return torch.mean(total_loss)
 
--- assignment1/cs336_basics/test.py
+++ assignment1/cs336_basics/test.py
@@ -10,21 +10,24 @@ class CrossEntropyLoss(nn.Module) :
         Notice that CE(x, target_idx) = - log softmax(x)[target_idx]
                                           = log(sum(e**x)) - log e**x[i]) = log(sum(e**x)) - x[i]
         """
+        target = target.to(torch.int64)
         # Minus max value for numerical stability
-        scale_logits = logits - torch.max(logits, dim= -1, keepdim= True).values
-        breakpoint() 
+        logits = logits - torch.max(logits, dim= -1, keepdim= True).values
+        # breakpoint() 
         
-        e_logits = torch.exp(scale_logits)
+        e_logits = torch.exp(logits)
         log_sum = torch.log(torch.sum(e_logits, dim=-1, keepdim= False))
         logits_idx = logits.gather(dim=-1, index= target.unsqueeze(-1)).squeeze(-1)
-
+        # breakpoint()
         total_loss = log_sum - logits_idx
         return torch.mean(total_loss)
 
 criteria = CrossEntropyLoss()
+true_criteria = nn.CrossEntropyLoss()
 
-logits = torch.Tensor([[2, 1, 2], [3, 1, 1]])
+logits = torch.Tensor([[2, 1, 3], [3, 1, 2], [1.2, 2.5, 1], [-5, 0, -10], [2020, 1, 2021]])
 
-targets = torch.Tensor([1, 0])
+targets = torch.Tensor([1, 0, 1, 2, 2])
 
+print(true_criteria(logits, targets.to(torch.int64)))
 print(criteria(logits, targets))
